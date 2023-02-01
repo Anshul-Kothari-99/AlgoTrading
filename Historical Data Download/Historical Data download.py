f80d883592586db3e15a04f5abd57530fb6a8ee9
anshul kothari <kotharianshul.1998@gmail.com>
2023-02-01 11:19:02 +0530

    NSE Symbols updated

--- Historical Data Download/Historical Data download.py
+++ Historical Data Download/Historical Data download.py
@@ -27,17 +27,41 @@ start_date = date_n_days_ago
 end_date = today_date
 timeframe = '5m'
 
-for ticker in tickers[:5]:
+failed_for = []
+
+temp = ['3IINFOTECH.NS', 'ADANIGAS.NS', 'ADHUNIKIND.NS', 'ADLABS.NS', 'AGCNET.NS', 'AGLSL.NS', 'AHLWEST.NS', 'AIONJSW.NS', 
+        'ALBK.NS', 'ANDHRABANK.NS', 'ANSALHSG.NS', 'ARIHANT.NS', 'ARROWTEX.NS', 'BARTRONICS.NS', 'BHARATFIN.NS', 
+        'BLUEBLENDS.NS', 'BOROSIL.NS', 'BSELINFRA.NS', 'CADILAHC.NS', 'CEBBCO.NS', 'CELESTIAL.NS', 'CESCVENT.NS', 
+        'CHROMATIC.NS', 'CIMMCO.NS', 'CNOVAPETRO.NS', 'CORPBANK.NS', 'COSMOFILMS.NS', 'COX&KINGS.NS', 'DEEPIND.NS', 
+        'DHFL.NS', 'DOLPHINOFF.NS', 'DQE.NS', 'EASUNREYRL.NS', 'EON.NS', 'ESSELPACK.NS', 'EXCELCROP.NS', 'FAIRCHEM.NS', 
+        'GALLISPAT.NS', 'GAMMNINFRA.NS', 'GARDENSILK.NS', 'GDL.NS', 'GLOBOFFS.NS', 'GRUH.NS', 'GSKCONS.NS', 'GTNIND.NS', 
+        'GUJFLUORO.NS', 'HARITASEAT.NS', 'HEXAWARE.NS', 'HIGHGROUND.NS', 'HINDSYNTEX.NS', 'HINDUJAVEN.NS', 'HOTELEELA.NS', 
+        'HSIL.NS', 'IBVENTURES.NS', 'INDIA VIX.NS', 'INFRATEL.NS', 'IPAPPM.NS', 'JMTAUTOLTD.NS', 'JUBILANT.NS', 'KARDA.NS', 
+        'KESARENT.NS', 'KSERASERA.NS', 'KSK.NS', 'KTIL.NS', 'KWALITY.NS', 'LAKSHVILAS.NS', 'LTI.NS', 'MAGMA.NS', 'MAJESCO.NS', 
+        'MANPASAND.NS', 'MAXINDIA.NS', 'MCDHOLDING.NS', 'MEGH.NS', 'MINDAIND.NS', 'MINDTREE.NS', 'MONSANTO.NS', 'MOTHERSUMI.NS', 
+        'MUKANDENGG.NS', 'NAGAROIL.NS', 'NBVENTURES.NS', 'NIITTECH.NS', 'NITESHEST.NS', 'NITINFIRE.NS', 'OMMETALS.NS', 
+        'OPTOCIRCUI.NS', 'ORIENTBANK.NS', 'ORIENTREF.NS', 'ORTINLABSS.NS', 'PAEL.NS', 'PAPERPROD.NS', 'PATSPINLTD.NS', 
+        'PDSMFL.NS', 'PHILIPCARB.NS', 'PRABHAT.NS', 'PRAKASHCON.NS', 'PROSEED.NS', 'PSL.NS', 'python_file_read.NS', 
+        'RNAM.NS', 'RTNINFRA.NS', 'SELMCL.NS', 'SHRIRAMCIT.NS', 'SHRIRAMEPC.NS', 'SICAGEN.NS', 'SORILINFRA.NS', 'SRIPIPES.NS', 
+        'STRTECH.NS', 'SUBEX.NS', 'SUPPETRO.NS', 'SYNDIBANK.NS', 'TALWALKARS.NS', 'TALWGYM.NS', 'TATAGLOBAL.NS', 
+        'TATASPONGE.NS', 'TATASTLBSL.NS', 'TCIDEVELOP.NS', 'TECHNOFAB.NS', 'TMRVL.NS', 'UNIPLY.NS', 'UNITEDBNK.NS', 
+        'UTTAMSTL.NS', 'UVSL.NS', 'VIKASMCORP.NS', 'WABCOINDIA.NS', 'WEIZFOREX.NS', 'ZODJRDMKJ.NS', 'ZUARIGLOB.NS']
+
+for ticker in tickers[:]:
     stock_data = yf.download(ticker[0], start = start_date, end = end_date, interval = timeframe)
-    print('Ticker Name -------------> ', ticker[0])
+    #print('Ticker Name -------------> ', ticker[0])
     #print(stock_data.shape[0])
     if stock_data.shape[0] > 0:
         stock_data = stock_data.reset_index()
         stock_data['Date'] = pd.to_datetime(stock_data['Datetime']).dt.date
         stock_data['Time'] = pd.to_datetime(stock_data['Datetime']).dt.time
         stock_data.drop(['Datetime', 'Volume', 'Adj Close'], axis=1, inplace=True)
-        print(stock_data.head())
-        print('\n')
+        #print(stock_data.head())
+        #print('\n')
         #print(stock_data.columns)
     else:
         print('Download Failing for -------------> ', ticker[0], '\n')
+        failed_for.append(ticker[0])
+        
+print('************************* Finished *************************\n')
+print(failed_for)
--- Historical Data Download/Temp_for_getting_YF_NSE_ticker_names.py
+++ Historical Data Download/Temp_for_getting_YF_NSE_ticker_names.py
@@ -4,30 +4,16 @@ Created on Wed Feb  1 00:16:13 2023
 
 @author: kotha
 """
-
-from os import walk
-import csv
 import pandas as pd
 
-mypath="D:\\Python Scripts\\shares Analysis\\reserve\\"
-files_list = next(walk(mypath), (None, None, []))[2] # this would extract all the files(shares) names present in the folder in a list
-
-#print(files_list)
-
-with open('C:\\Users\\kotha\\OneDrive\\Desktop\\Algo Trading Repo\\Historical Data Download\\YF_Ticker_Names.csv', 'w') as TempFile:
-    # using csv.writer method from CSV package
-    write = csv.writer(TempFile)      
-    write.writerow(files_list)
-    
-print("Done!!")
-ticker_names = pd.read_csv('C:\\Users\\kotha\\OneDrive\\Desktop\\Algo Trading Repo\\Historical Data Download\\YF_Ticker_Names.csv', header=None).T
-
+ticker_names = pd.read_csv('C:\\Users\\kotha\\OneDrive\\Desktop\\Algo Trading Repo\\Historical Data Download\\NSE_Symbols.csv')
 ticker_names['prefix'] = '.NS'
 ticker_names.columns = ['Ticker_name', 'Prefix']
-ticker_names[['Ticker_name', 'delete']] = ticker_names['Ticker_name'].str.split('.', 1, expand=True)
+#ticker_names[['Ticker_name', 'delete']] = ticker_names['Ticker_name'].str.split('.', 1, expand=True)
 ticker_names['YF_Ticker_Name'] = ticker_names['Ticker_name'] + ticker_names['Prefix']
-ticker_names.drop(['delete', 'Ticker_name', 'Prefix'], axis=1, inplace = True)
+ticker_names.drop(['Ticker_name', 'Prefix'], axis=1, inplace = True)
 #print(ticker_names)
 #print(ticker_names.columns)
 
 ticker_names.to_csv("YF_Ticker_Names.csv", index=False, header=False)
+print('Done')
